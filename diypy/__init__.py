004e8ef6094575ff278bad2823ae2aa5a3a2723f
AnqurVanillapy <anqurvanillapy@gmail.com>
2015-12-06 19:48:37 +0800

    Updated args exceptions

--- diypy/__init__.py
+++ diypy/__init__.py
@@ -18,11 +18,13 @@ class Diypy(object):
     def __init__(self):
 
         self.PREORDER = 0
+        self.INORDER = 1
+        self.POSTORDER = 2
 
-    def binary_tree(self, flag, *args):
+    def binary_tree(self, flag, args):
 
-        """Convert an args tuple to C string in a specific format to
-        _diypy._binary_tree() function. User can only create a binary
+        """Convert an args tuple or list to C string in specific format
+        to _diypy._binary_tree() function. User can only create a binary
         tree with less than 1023 nodes, whose types of data can only be
         string.
 
@@ -30,10 +32,17 @@ class Diypy(object):
         be removed. A good formatted C string can be 'foo bar ', which
         will create a binary tree with 2 nodes.
 
-        The first flag defines the order of input data, which includes:
-            LEVELORDER -- level-order
+        The first flag defines the order of input and output data, which
+        includes:
             PREORDER -- pre-order
+            INORDER -- in-order
+            POSTORDER -- post-order
         """
+
+        argtp = type(args).__name__
+        if argtp not in ['list', 'tuple']:
+            tperr = 'A list or tuple is required, not {0}'
+            raise TypeError(tperr.format(argtp))
 
         bt_str = ''
         for node in args:
